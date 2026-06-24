5ef32687858ccc5fd7678ad08533b77ba4c3eaaa
Satyam Swain <satyamswain38@gmail.com>
2026-06-25 00:41:18 +0530

    Completed Factory Method Pattern

---
+++ Week1/DesignPatterns/FactoryMethodPattern/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+// Product Interface
+public interface IDocument
+{
+    void Open();
+}
+
+// Concrete Products
+public class WordDocument : IDocument
+{
+    public void Open()
+    {
+        Console.WriteLine("Opening Word Document");
+    }
+}
+
+public class PdfDocument : IDocument
+{
+    public void Open()
+    {
+        Console.WriteLine("Opening PDF Document");
+    }
+}
+
+public class ExcelDocument : IDocument
+{
+    public void Open()
+    {
+        Console.WriteLine("Opening Excel Document");
+    }
+}
+
+// Creator Abstract Class
+public abstract class DocumentFactory
+{
+    public abstract IDocument CreateDocument();
+}
+
+// Concrete Factories
+public class WordFactory : DocumentFactory
+{
+    public override IDocument CreateDocument()
+    {
+        return new WordDocument();
+    }
+}
+
+public class PdfFactory : DocumentFactory
+{
+    public override IDocument CreateDocument()
+    {
+        return new PdfDocument();
+    }
+}
+
+public class ExcelFactory : DocumentFactory
+{
+    public override IDocument CreateDocument()
+    {
+        return new ExcelDocument();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DocumentFactory factory;
+
+        factory = new WordFactory();
+        IDocument doc1 = factory.CreateDocument();
+        doc1.Open();
+
+        factory = new PdfFactory();
+        IDocument doc2 = factory.CreateDocument();
+        doc2.Open();
+
+        factory = new ExcelFactory();
+        IDocument doc3 = factory.CreateDocument();
+        doc3.Open();
+    }
+}
